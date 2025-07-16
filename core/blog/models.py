3fffd8555375e2a105e641c0c1318570c40867a3
Asadbek <asadbekusmonov382@gmail.com>
2025-07-16 18:06:04 +0500

    al

--- core/blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class UserData(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
